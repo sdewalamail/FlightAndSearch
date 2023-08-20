ae4caeb84981abfbb664b274ddd527220a921f65
sdewalamail <sdewalamail@gmail.com>
2023-08-20 13:26:34 +0530

    bug fixed for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   },
   rules: {
     "quotes": ["error", "double", { "allowSingleQuotes": true }],
+    "no-undef": "error", // This rule will check for undefined variables
     // You can add or override other rules here as needed
   },
 };
--- src/routes/v1/airport.js
+++ src/routes/v1/airport.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 
 const routes = express.Router();
 
-const AIRPORT = require("../../controller/airport");
+const AIRPORT = require('../../controller/airport');
 
 routes.get("/getAll", AIRPORT.getAllAirport);
 routes.get("/:airportId", AIRPORT.get);
-routes.post("/create", AIRPORT.create);
+routes.post("/create", AIRPORT.create); 
 routes.post("/bulkcreate", AIRPORT.bulkCreate);
 routes.delete("/delete/:airportId", AIRPORT.destroy);
 routes.patch("/update/:airportId", AIRPORT.update);
